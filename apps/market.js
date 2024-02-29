ba602e23b95cb9057dda6ca11d61739bb9182416
wu985215993 <985215993@qq.com>
2024-03-01 02:51:04 +0800

    feat: 增加昨日收盘价显示

--- apps/market.js
+++ apps/market.js
@@ -97,9 +97,9 @@ export class Market extends plugin {
     // 输出结果
     const formatMsg = `${coinName} (${changePercent}%)
 当前价格:    ${lastPrice}
-支撑:         ${support}
-压力:         ${resistance}
-中轴:         ${pivot}
+支撑:          ${support}
+压力:          ${resistance}
+中轴:          ${pivot}
 昨日收盘价：${result[0].closePrice}
 `
     e.reply(formatMsg, true, { recallMsg: 0 }, true)
